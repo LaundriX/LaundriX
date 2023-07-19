34010ad647bb61bfcf99d8489141b1a36ccab545
NightFury742 <eshaan122@gmail.com>
2023-07-19 17:52:09 +0530

    setup react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
+import OrderList from './pages/OrderList';
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/OrderList" element={<OrderList />} />
+      </Routes>
     </>
   );
 }
