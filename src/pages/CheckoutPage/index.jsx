2f278df9fb9213b485dd8237511a852e8232d6d9
jainvedant392 <jainvedant392@gmail.com>
2023-07-26 19:44:08 +0530

    modify: set navigation between pages

--- src/pages/CheckoutPage/index.jsx
+++ src/pages/CheckoutPage/index.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import Navbar from '../../components/Navbar';
-import { Box } from '@chakra-ui/react';
+import { Box, Button } from '@chakra-ui/react';
+import { useNavigate } from 'react-router-dom';
 
 const CheckoutPage = () => {
+  const navigate = useNavigate();
   return (
     <>
       <Navbar />
       <Box mt="75px">CheckOutPage</Box>
+      <Button
+        onClick={() => {
+          navigate('/OrderConfirmationPage');
+        }}
+      >
+        Pay and Confirm
+      </Button>
     </>
   );
 };
